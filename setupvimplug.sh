375c4059e0764f037a9fc7236b210c2cf4e5a085
thahmidul-nafi-primesilicon <thahmidul.nafi@primesilicon.com>
2022-09-12 16:50:36 +0600

    created a bash file for insta setup. Needs to be refined.

---
+++ setupvimplug.sh
@@ -0,0 +1,11 @@
+mkdir ~/.config/nvim mkdir ~/.config/nvim/autoload mkdir ~/.config/nvim/vim-plug
+
+git clone https://github.com/thahmidul-nafi-primesilicon/vim-config.git
+
+curl -fLo ~/.var/app/io.neovim.nvim/data/nvim/autoload/plug.vim --create-dirs
+https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
+cp ~/vim-config/init.vim ~/.config/nvim/init.vim
+cp ~/vim-config/init.vim ~/.config/nvim/vim-plug/plugins.vim
+
+
